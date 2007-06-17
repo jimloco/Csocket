51ac9d9a6b36809fab657dd805ceb3a2c1a39e2a
imaginos <imaginos>
2007-06-17 22:00:09 +0000

    fix copyright

--- Csocket.cc
+++ Csocket.cc
@@ -1,6 +1,6 @@
 /** @file
 *
-*    Copyright (c) 1999-2006 Jim Hull <imaginos@imaginos.net>
+*    Copyright (c) 1999-2007 Jim Hull <imaginos@imaginos.net>
 *    All rights reserved
 *
 * Redistribution and use in source and binary forms, with or without modification,
@@ -28,7 +28,7 @@
 * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *
-* $Revision: 1.51 $
+* $Revision: 1.52 $
 */
 
 #include "Csocket.h"
