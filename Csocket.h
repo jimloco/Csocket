1fe5d499faa0f285784d6018b3d343f4b1b489c8
imaginos <imaginos>
2009-06-24 19:30:53 +0000

    make Select protected

--- Csocket.h
+++ Csocket.h
@@ -28,7 +28,7 @@
 * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *
-* $Revision: 1.208 $
+* $Revision: 1.209 $
 */
 
 // note to compile with win32 need to link to winsock2, using gcc its -lws2_32
@@ -1818,12 +1818,13 @@ public:
 		return( iRet );
 	}
 
-private:
+protected:
 	//! this is a strict wrapper around C-api select(). Added in the event you need to do special work here
 	virtual int Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout)
 	{
 		return( select( nfds, readfds, writefds, exceptfds, timeout ) );
 	}
+private:
 	/**
 	* fills a map of socks to a message for check
 	* map is empty if none are ready, check GetErrno() for the error, if not SUCCESS Select() failed
