4da62b2c6a306488674ece0ec92dc1cc73e2065f
Jim Hull <csocket@jimloco.com>
2011-09-16 19:01:50 -0700

    tweaked to work with new system of monitoring by Csock

---
+++ CurlSock.cc
@@ -0,0 +1,219 @@
+/**
+*
+*    Copyright (c) 1999-2011 Jim Hull <imaginos@imaginos.net>
+*    All rights reserved
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list
+* of conditions and the following disclaimer in the documentation and/or other materials
+*  provided with the distribution.
+* Redistributions in any form must be accompanied by information on how to obtain
+* complete source code for this software and any accompanying software that uses this software.
+* The source code must either be included in the distribution or be available for no more than
+* the cost of distribution plus a nominal fee, and must be freely redistributable
+* under reasonable conditions. For an executable file, complete source code means the source
+* code for all modules it contains. It does not include source code for modules or files
+* that typically accompany the major components of the operating system on which the executable file runs.
+*
+* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+* BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+* OR NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THIS SOFTWARE BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*
+*/
+#include "CurlSock.h"
+
+#ifndef _NO_CSOCKET_NS
+namespace Csocket
+{
+#endif /* _NO_CSOCKET_NS */
+
+CCurlSock::CCurlSock() 
+{
+	m_iTimeout_ms = -1;
+	m_pMultiHandle = NULL;
+}
+
+CCurlSock::~CCurlSock() 
+{	
+	for( map< CURL *, bool >::iterator it = m_pcbCurlHandles.begin(); it != m_pcbCurlHandles.end(); ++it )
+	{
+		CURL * pCurl = it->first;
+		if( m_pMultiHandle )
+			curl_multi_remove_handle( m_pMultiHandle, pCurl );
+		curl_easy_cleanup( pCurl );
+	}
+	m_pcbCurlHandles.clear();
+	if( m_pMultiHandle )
+	{
+		curl_multi_cleanup( m_pMultiHandle );
+		m_pMultiHandle = NULL;
+	}
+}
+
+bool CCurlSock::GatherFDsForSelect( std::map< int, short > & miiReadyFds, long & iTimeoutMS )
+{
+	if( m_pMultiHandle )
+	{
+		// look for any timeout changes, and any fd changes
+		int iRunningHandles = 0;
+		curl_multi_socket_action( m_pMultiHandle, CURL_SOCKET_TIMEOUT, 0, &iRunningHandles );
+
+		if( iRunningHandles >= 1 )
+		{ // this means there is a request working
+
+			if( m_miiMonitorFDs.size() ) 
+			{ // the call back came through and there are fd's to work on
+
+				// cycle through the available fd's and get the specific actions needed to proceed
+				for( std::map< int, short >::iterator it = m_miiMonitorFDs.begin(); it != m_miiMonitorFDs.end(); ++it )
+					curl_multi_socket_action( m_pMultiHandle, it->first, 0, &iRunningHandles );
+
+				// cycle through any additional fd's and set them into Csocket to monitor
+				for( std::map< int, short >::iterator it = m_miiMonitorFDs.begin(); it != m_miiMonitorFDs.end(); ++it )
+				{
+					if( it->second > 0 )
+						miiReadyFds[it->first] = it->second;
+				}
+			}
+
+			// change the timeout if reqested to do so
+			if( m_iTimeout_ms >= 0 )
+				iTimeoutMS = m_iTimeout_ms;
+		}
+		// check for anything complete
+		CURLMsg * pMSG = NULL;
+		int iNumMsgQueue = 0;
+		do
+		{
+			pMSG = curl_multi_info_read( m_pMultiHandle, &iNumMsgQueue );
+			if( pMSG && pMSG->msg == CURLMSG_DONE )
+			{
+				OnCURLComplete( pMSG->easy_handle );	
+				m_pcbCurlHandles.at( pMSG->easy_handle ) = false;
+			}
+		}
+		while( pMSG && iNumMsgQueue > 0 );
+
+	}
+
+	return( m_bEnabled );
+}
+
+CURL * CCurlSock::Retr( const CS_STRING & sURL )
+{
+	if( !m_pMultiHandle )
+	{
+		m_pMultiHandle = curl_multi_init();
+		// assign the next functions to get information about the internal fd's and the suggested timeout
+		curl_multi_setopt( m_pMultiHandle, CURLMOPT_SOCKETFUNCTION, CCurlSock::SetupSock );
+		curl_multi_setopt( m_pMultiHandle, CURLMOPT_SOCKETDATA, this );
+		curl_multi_setopt( m_pMultiHandle, CURLMOPT_TIMERFUNCTION, CCurlSock::SetupTimer );
+		curl_multi_setopt( m_pMultiHandle, CURLMOPT_TIMERDATA, this );
+	}
+	CURL * pCURL = NULL;
+	for( map< CURL *, bool >::iterator it = m_pcbCurlHandles.begin(); it != m_pcbCurlHandles.end(); ++it )
+	{
+		if( !it->second )
+		{
+			pCURL = it->first;
+			it->second = true; // mark it as used
+			curl_easy_reset( pCURL );
+			break;
+		}
+	}
+
+	if( !pCURL )
+	{
+		pCURL = curl_easy_init();
+		m_pcbCurlHandles[pCURL] = true;
+	}
+	// prepare the handle, this is just a proof of concept, so doing it right here
+	// you can create a CURL handle for each query, re-use them (to persist connections), drop them off, etc
+	// the easiest method if you are serially retrieving documents is to do them through one CURL handle
+	// otherwise you can make a pool of handles, etc using curl_multi_info_read to see which is ready and so forth
+curl_easy_setopt( pCURL, CURLOPT_VERBOSE, 1 );
+	curl_easy_setopt( pCURL, CURLOPT_FOLLOWLOCATION, 1 );
+	curl_easy_setopt( pCURL, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1 );
+	curl_easy_setopt( pCURL, CURLOPT_ENCODING, "" );
+	curl_easy_setopt( pCURL, CURLOPT_SSL_VERIFYPEER, 0 );
+	curl_easy_setopt( pCURL, CURLOPT_WRITEFUNCTION, CCurlSock::WriteData );
+	curl_easy_setopt( pCURL, CURLOPT_HEADERFUNCTION, CCurlSock::WriteHeader );
+
+	// send curl back as the argument to the functions,
+	// and tie this class as a reference to that object for function calls
+	curl_easy_setopt( pCURL, CURLOPT_WRITEDATA, pCURL );
+	curl_easy_setopt( pCURL, CURLOPT_WRITEHEADER, pCURL );
+	curl_easy_setopt( pCURL, CURLOPT_PRIVATE, this );
+	curl_multi_add_handle( m_pMultiHandle, pCURL );
+
+	curl_easy_setopt( pCURL, CURLOPT_URL, sURL.c_str() );
+	return( pCURL );
+}
+
+size_t CCurlSock::WriteData( void * pData, size_t uSize, size_t uNemb, void * pCBPtr )
+{
+	CURL *pCURL = static_cast< CURL * >( pCBPtr );
+	CCurlSock * pManager = NULL;
+	if( curl_easy_getinfo( pCURL, CURLINFO_PRIVATE, &pManager ) != CURLE_OK )
+		return( 0 );
+	assert( pManager );
+	size_t uBytes = uSize * uNemb;
+	return( pManager->OnBody( pCURL, (const char *)pData, uBytes ) );
+}
+
+size_t CCurlSock::WriteHeader( void * pData, size_t uSize, size_t uNemb, void * pCBPtr )
+{
+	CURL *pCURL = static_cast< CURL * >( pCBPtr );
+	CCurlSock * pManager = NULL;
+	if( curl_easy_getinfo( pCURL, CURLINFO_PRIVATE, &pManager ) != CURLE_OK )
+		return( 0 );
+	assert( pManager );
+	size_t uBytes = uSize * uNemb;
+	return( pManager->OnHeader( pCURL, (const char *)pData, uBytes ) );
+}
+
+int CCurlSock::SetupSock( CURL * pCurl, curl_socket_t iFD, int iWhat, void * pCBPtr, void * pSockPtr )
+{
+	CCurlSock *pManager = static_cast< CCurlSock * >( pCBPtr );
+	if( iWhat == CURL_POLL_IN )
+	{
+		pManager->Add( iFD, CSocketManager::ECT_Read );
+	}
+	else if( iWhat == CURL_POLL_OUT )
+	{
+		pManager->Add( iFD, CSocketManager::ECT_Write );
+	}
+	else if( iWhat == CURL_POLL_INOUT )
+	{
+		pManager->Add( iFD, CSocketManager::ECT_Write|CSocketManager::ECT_Read );
+	}
+	else if( iWhat == CURL_POLL_REMOVE )
+	{
+		pManager->Remove( iFD );
+	}
+	else
+	{
+		pManager->Add( iFD, 0 );
+	}
+	return( 0 );
+}
+
+int CCurlSock::SetupTimer( CURLM * pMulti, long iTimeoutMS, void *pCBPtr )
+{
+	CCurlSock *pManager = static_cast< CCurlSock * >( pCBPtr );
+	pManager->SetTimeoutMS( iTimeoutMS );
+	return( 0 );
+}
+
+#ifndef _NO_CSOCKET_NS
+};
+#endif /* _NO_CSOCKET_NS */
---
+++ CurlSock.h
@@ -0,0 +1,101 @@
+/**
+*
+*    Copyright (c) 1999-2011 Jim Hull <imaginos@imaginos.net>
+*    All rights reserved
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list
+* of conditions and the following disclaimer in the documentation and/or other materials
+*  provided with the distribution.
+* Redistributions in any form must be accompanied by information on how to obtain
+* complete source code for this software and any accompanying software that uses this software.
+* The source code must either be included in the distribution or be available for no more than
+* the cost of distribution plus a nominal fee, and must be freely redistributable
+* under reasonable conditions. For an executable file, complete source code means the source
+* code for all modules it contains. It does not include source code for modules or files
+* that typically accompany the major components of the operating system on which the executable file runs.
+*
+* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+* BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+* OR NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THIS SOFTWARE BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*
+*/
+#ifndef HAVE_CURLSOCK_H
+#define HAVE_CURLSOCK_H
+
+#include "Csocket.h"
+#include <curl/curl.h>
+
+#ifndef _NO_CSOCKET_NS
+namespace Csocket
+{
+#endif /* _NO_CSOCKET_NS */
+/**
+ * @brief Csocket style wrapper around libcurl-multi
+ *
+ * http://curl.haxx.se/libcurl/c/libcurl-multi.html
+ * This isn't finished, but this is my basic approach. I added a means to which Csocket can
+ * monitor file descriptors it doesn't directly control. This class hooks into that by virtue
+ * of a callback "GatherFDsForSelect" and ties that libcurl-multi. When Csocket looks for fd's to 
+ * monitor, this class calls "curl_multi_socket_action" which does a couple things ...
+ * - 1. Calls our CURL callback CCurlSock::SetupSock to let us know the FD's and what they should be monitored for
+ * - 2. Calls our CURL callback CCurlSock::SetupTimer which lets us know the maxium wait time for activity
+ * - 3. Process any handles that are in a ready state
+ *
+ * The end point here is a non-blocking method to fetch documents via CURL within Csocket
+ */
+class CCurlSock : public CSMonitorFD
+{
+public:
+	CCurlSock(); 
+	virtual ~CCurlSock();
+
+	//! the hook we tie into to get events from csocket
+	virtual bool GatherFDsForSelect( std::map< int, short > & miiReadyFds, long & iTimeoutMS );
+	//! reimplement this to do nothing as its not needed
+	virtual bool CheckFDs( const std::map< int, short > & miiReadyFds ) { return( m_bEnabled ); }
+
+	/**
+	 * @brief the main trigger to fetch a document. This could change, but so far this is what I have
+	 * @param sURL the location of the document
+	 * @return a CURL object tied to that document which you can use to configure any additional tweaks to CURL before the request is sent
+	 */
+	CURL * Retr( const CS_STRING & sURL );
+protected:
+	//! called when the transfer associate with this CURL object is completed
+	virtual void OnCURLComplete( CURL * pCURL ) { m_bEnabled = false; }
+
+
+	//! called as header information for the document is returned
+	size_t OnHeader( CURL * pCURL, const char * pData, size_t uBytes ) { return( uBytes ); }
+	//! called as the document is returned
+	size_t OnBody( CURL * pCURL, const char * pData, size_t uBytes ) { return( uBytes ); }
+private:
+	static size_t WriteData( void * pData, size_t uSize, size_t uNemb, void * pStream );
+	static size_t WriteHeader( void * pData, size_t uSize, size_t uNemb, void * pStream );
+	static int SetupSock( CURL * pCurlHandle, curl_socket_t iFD, int iWhat, void * pcbPtr, void * pSockPtr );
+	static int SetupTimer( CURLM * pMulti, long iTimeout_ms, void * pcdPtr );
+
+	void SetTimeoutMS( long iTimeoutMS )
+	{
+		m_iTimeout_ms = iTimeoutMS;
+	}
+
+	long m_iTimeout_ms; //!< current timeout to use
+	CURLM	* m_pMultiHandle; //!< the main multi handle
+	map< CURL *, bool > m_pcbCurlHandles; //!< map of CURL objects to bools. If the bool is true then that object is in use
+};
+#ifndef _NO_CSOCKET_NS
+};
+#endif /* _NO_CSOCKET_NS */
+
+#endif /* HAVE_CURLSOCK_H */
